f961c52229e3a03bce04539049b1525e5321e151
FlamingHerb <msbbomisol@gmail.com>
2026-07-21 00:44:00 +0800

    feat: restore HTML structure in index.php and remove JSON response

--- api/index.php
+++ api/index.php
@@ -1,4 +1,21 @@
-<?php
-
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HoloTEMPUS HQ 4th Anniversary Celebration</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="/js/script.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&family=Winky+Sans:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include __DIR__ . "/../header.html"; ?>
+    <main>
+        <img class='background-plate' src="/01_background/1_concerthall.png" loading="lazy">
+    </main>
+    <?php include __DIR__ . "/../footer.html"; ?>
+</body>
+</html>
