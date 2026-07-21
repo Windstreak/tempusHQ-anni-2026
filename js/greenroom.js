271c75726740d08bd9af9d8deb2ab10a578ea60b
Windstreak <jonnyboyx2006@gmail.com>
2026-07-21 10:51:10 -0700

    Added entries 09, 10 to greenroom

--- js/greenroom.js
+++ js/greenroom.js
@@ -9,9 +9,9 @@ const entryData = await loadCSV(CSVPath);
 $(placeOverlay());
 
 $(async function placeEntries() {
-    for (let i = 1; i <= 8; i++){
+    for (let i = 1; i <= 10; i++){
         let number = i < 10? '0'+i : i;
-        if (i!=3)
+        if (i!=3&&i!=9&&i!=10)
         {
             $(".entries").append('<img class="entry art" id="entry_'+number+'" src='+iconFilePath+number+'_entry_icon.png loading="lazy">');
             let entry = document.querySelector('#entry_'+number);
